0eb1bf1cd22b86b3637c25a37885629ed8c3e13b
Vikash8513 <vikashpaswan765@gmail.com>
2026-08-05 08:10:44 +0530

    Formatted Basic SELECT Practice

--- 01_Basic_SELECT.sql
+++ 01_Basic_SELECT.sql
@@ -1,1 +1,26 @@
-select * from employee
+/*
+=================================================================================================================
+Topic      : Basic SELECT
+Database   : Employee Management System
+SQL Server : Microsoft SQL Server
+=================================================================================================================
+*/
+
+-- Question 1.
+-- Display all Employees.
+
+SELECT *
+FROM employee;
+-------------------------------------------------------------------------------------------------------------------
+-- Question 2.
+-- Display Employee_Id, Name, Department, and Salary.
+SELECT Employee_Id,
+    Name,
+    Department,
+    Salary 
+FROM Employee;
+------------------------------------------------------------------------------------------------------------------
+
+
+
+
